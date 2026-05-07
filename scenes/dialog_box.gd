d43915ab6e68b848d2def72610ac4e722c156e66
bythedoor <bythedoor115@gmail.com>
2026-05-07 18:12:06 +0200

    feat : added choice dialog box

---
+++ scenes/dialog_box.gd
@@ -0,0 +1,45 @@
+extends PanelContainer
+
+signal SELECTED(index)
+
+@onready var choices_list = $"MarginContainer/Choices"
+@onready var choice_default = $"MarginContainer/Choices/ChoiceButton"
+
+# list of choices available to the player
+var choices:
+	set(value):
+		choices = value
+		initButtons()
+	
+	
+# Called when: the node enters the scene tree for the first time.
+func _ready() -> void:
+	choices_list.get_child(0).pressed.connect(onChoice.bind(0))
+
+func onChoice(index):
+	visible = false
+	SELECTED.emit(index)
+	
+func initButtons():
+	var button
+	
+	# deleting old buttons
+	while choices_list.get_child_count() > 1:
+		button = choices_list.get_child(choices_list.get_child_count() - 1)
+		choices_list.remove_child(button)
+		button.queue_free()
+	
+	# adding the new ones
+	for index in range(choices.size()):
+		if (index == 0):
+			choices_list.get_child(0).text = choices[index]
+		else:
+			choices_list.add_child(choice_default.duplicate())
+			choices_list.get_child(index).text = choices[index]
+			choices_list.get_child(index).pressed.connect(onChoice.bind(index))
+		
+		
+		
+		
+		
+	
--- scripts/main_interface.gd
+++ scripts/main_interface.gd
@@ -2,10 +2,12 @@ extends Node2D
 
 # node connection
 @onready var mimi: Sprite2D = $mimi
+@onready var dialogBox = $DialogBox
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	dialogBox.choices = ["go to sleep.", "do nothing."]
+	dialogBox.visible = true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
